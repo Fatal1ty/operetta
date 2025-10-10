f986691243b747b0358df1b587241778690a1d36
Alexander Tikhonov <random.gauss@gmail.com>
2025-10-10 21:11:36 +0300

    Make it compatible with older Pythons

--- operetta/service/base.py
+++ operetta/service/base.py
@@ -1,14 +1,17 @@
 from abc import ABC
-from typing import Sequence
+from typing import Generic, Sequence, TypeVar
 
 import aiomisc
 import dishka
 
 
-class Service(aiomisc.Service, ABC):
+T = TypeVar("T")
+
+
+class Service(aiomisc.Service, ABC, Generic[T]):
     async def get_di_providers(self) -> Sequence[dishka.Provider]:
         return []
 
-    async def get_dependency[T](self, dependency_type: type[T]) -> T:
+    async def get_dependency(self, dependency_type: type[T]) -> T:
         container: dishka.Container = await self.context["dishka_container"]
         return await container.get(dependency_type)
